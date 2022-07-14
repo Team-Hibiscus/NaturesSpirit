7ac0230f685df96574ad7168ec1ae9a602c3a8fd
Eko-byte <85321665+Eko-byte@users.noreply.github.com>
2022-07-13 21:10:32 -0500

    Initial commit

---
+++ src/main/java/net/hibiscus/naturespirit/NatureSpirit.java
@@ -0,0 +1,17 @@
+package net.hibiscus.naturespirit;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NatureSpirit implements ModInitializer {
+
+    public static final String MOD_ID = "hibiscus";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
