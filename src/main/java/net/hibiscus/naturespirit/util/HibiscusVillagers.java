aa58755e549aa965b208344cce7e85806e5cef77
Eko-byte <85321665+Eko-byte@users.noreply.github.com>
2023-08-15 17:21:31 -0500

    Adobe Villages

--- src/main/java/net/hibiscus/naturespirit/util/HibiscusVillagers.java
+++ src/main/java/net/hibiscus/naturespirit/util/HibiscusVillagers.java
@@ -11,9 +11,13 @@ import static net.minecraft.village.VillagerType.BIOME_TO_TYPE;
 public class HibiscusVillagers {
    public static final VillagerType WISTERIA = VillagerTypeHelper.register(new Identifier(MOD_ID, "wisteria"));
    public static final VillagerType CYPRESS = VillagerTypeHelper.register(new Identifier(MOD_ID, "cypress"));
+   public static final VillagerType ADOBE = VillagerTypeHelper.register(new Identifier(MOD_ID, "adobe"));
    public static void registerVillagers() {
       BIOME_TO_TYPE.put(HibiscusBiomes.WISTERIA_FOREST, WISTERIA);
       BIOME_TO_TYPE.put(HibiscusBiomes.CYPRESS_FIELDS, CYPRESS);
       BIOME_TO_TYPE.put(HibiscusBiomes.CARNATION_FIELDS, CYPRESS);
+      BIOME_TO_TYPE.put(HibiscusBiomes.STRATIFIED_DESERT, ADOBE);
+      BIOME_TO_TYPE.put(HibiscusBiomes.LIVELY_DUNES, ADOBE);
+      BIOME_TO_TYPE.put(HibiscusBiomes.BLOOMING_DUNES, ADOBE);
    };
 }
