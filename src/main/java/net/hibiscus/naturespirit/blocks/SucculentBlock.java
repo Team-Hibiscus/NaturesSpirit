fa06a92565bcb6315541faccbdcedc32e2f2fa2a
Eko-byte <85321665+Eko-byte@users.noreply.github.com>
2024-07-21 22:26:22 -0400

    Suc
    Succulent Renewability
    Joshua Sapling Fix

--- src/main/java/net/hibiscus/naturespirit/blocks/SucculentBlock.java
+++ src/main/java/net/hibiscus/naturespirit/blocks/SucculentBlock.java
@@ -8,12 +8,13 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
 
-public class SucculentBlock extends CoralParentBlock {
+public class SucculentBlock extends CoralParentBlock implements Fertilizable{
    public static final MapCodec<SucculentBlock> CODEC = SucculentBlock.createCodec(SucculentBlock::new);
    private static final VoxelShape SHAPE = Block.createCuboidShape(2.0, 0.0, 2.0, 14.0, 4.0, 14.0);
 
@@ -42,6 +43,18 @@ public class SucculentBlock extends CoralParentBlock {
    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
       BlockPos blockPos = pos.down();
       return world.getBlockState(blockPos).isSideSolid(world, blockPos, Direction.UP, SideShapeType.CENTER) || world.getBlockState(blockPos).isIn(HibiscusTags.Blocks.SUCCULENT_VERTICAL_PLACEMENT_OVERRIDE);
+   }
+
+   @Override public boolean isFertilizable(WorldView world, BlockPos pos, BlockState state, boolean isClient) {
+      return true;
+   }
+
+   @Override public boolean canGrow(World world, Random random, BlockPos pos, BlockState state) {
+      return true;
+   }
+
+   @Override public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
+      dropStack(world, pos, new ItemStack(this));
    }
 }
 
